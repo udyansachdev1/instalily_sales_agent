46ab379986212c013a1d6decf225d96746cc9e01
udyansachdev1 <143528446+udyansachdev1@users.noreply.github.com>
2025-03-24 04:24:35 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,4 @@ CLAY_API_KEY = "your_clay_api_key"
 
 EVENTBRITE_API_KEY = "VBQEC4SZXJ2R2QNTNQ"
 
-api_key = "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"
-
 hunter_api_key = "d14e41a2cf1953e63c49dd23a6d243c7677f1d5d"
