9be3128817fa9c58d8702b9ce2d27e7320ba0ea5
udyansachdev1 <143528446+udyansachdev1@users.noreply.github.com>
2025-03-24 12:20:35 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 import streamlit as st
 
 # Retrieve API key from environment variable
-api_key = st.secrets.general.API_KEY
-
+#api_key = st.secrets.general.API_KEY
+api_key = os.getenv("API_KEY")
 LINKEDIN_API_KEY = "your_linkedin_api_key"
 CLAY_API_KEY = "your_clay_api_key"
 
